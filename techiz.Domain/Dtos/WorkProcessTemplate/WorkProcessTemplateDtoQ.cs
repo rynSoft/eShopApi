fa5097799882b8b6a0c325dcf4f936937cef5acc
salihdurus <durussalih@gmail.com>
2024-03-13 03:01:59 +0300

    name get private a çekildi active propu eklendi

--- techiz.Domain/Dtos/WorkProcessTemplate/WorkProcessTemplateDtoQ.cs
+++ techiz.Domain/Dtos/WorkProcessTemplate/WorkProcessTemplateDtoQ.cs
@@ -7,12 +7,13 @@ namespace techiz.Domain.Dtos;
 public class WorkProcessTemplateDtoQ
 {
     public string Id { get; set; }
-    public string Name { get; set; }
+    public string Name { private get; set; }
     public string Content { get { return Name; } }
     public string Version { get; set; }
     public string WhichPage { get; set; }
     public string Color { get; set; }
     public string Icon { get; set; }
+    public bool Active { get; set; }
     public int WorkProcessTemplateId { get; set; }
     public List<WorkProcessRouteUserDtoQ> UserList { get; set; }
 }
