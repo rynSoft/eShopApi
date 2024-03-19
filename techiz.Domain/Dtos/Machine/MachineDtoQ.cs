172b82d4b961a0fd88b329ceb9889f0a9d8db8bd
salihdurus <durussalih@gmail.com>
2024-03-20 00:40:12 +0300

    MachineDto OutputCameraId prop added

--- techiz.Domain/Dtos/Machine/MachineDtoQ.cs
+++ techiz.Domain/Dtos/Machine/MachineDtoQ.cs
@@ -9,6 +9,7 @@ public class MachineDtoQ
     public int LineId { get; set; }
     public string LineAd { get; set; }
     public int? InputCameraId { get; set; }
+    public int? OutputCameraId { get; set; }
     public string Code { get; set; }
     public string Name { get; set; }
     public string? BarcodeReaderId { get; set; }
